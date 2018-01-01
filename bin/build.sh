b6764779a440065fae934e2b4915a529cff4487c
kitsuyui <kitsuyui@kitsuyui.com>
2018-01-01 16:21:43 +0900

    Remove dependency to CGO in statically linked binary
    - This makes more compatibility with Alpine Linux.

--- bin/build.sh
+++ bin/build.sh
@@ -2,4 +2,4 @@
 go get -d ./...
 go get -d github.com/inconshreveable/mousetrap
 go get -d github.com/Microsoft/go-winio
-gox -ldflags '-w -s' -output='build/scraper_{{.OS}}_{{.Arch}}'
+CGO_ENABLE=0 gox -ldflags '-w -s' -output='build/scraper_{{.OS}}_{{.Arch}}'
